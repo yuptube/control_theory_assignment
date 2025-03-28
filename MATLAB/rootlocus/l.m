9409a8b17a4ded44cd1e6d7c9334d3d4074b2573
yuptube <joshuaszd@gmail.com>
2025-03-28 11:58:47 +0000

    del unnes file

--- MATLAB/rootlocus/l.m
+++
@@ -1,8 +0,0 @@
-t = 0:0.01:10; % Define time vector
-G=tf([1,3], [1,4, 5])
-[y, t] = step(G, t); % Compute step response
-disp(t);
-plot(t, y); grid on;
-xlabel('Time (s)');
-ylabel('Response');
-title('Step Response');
